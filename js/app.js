6083f5491d9c429111d1518494f5cfe06bdcaa23
Arunbharathi03 <arunaravind03@gmail.com>
2021-12-23 16:02:41 +0530

    mobile first style for menu and story

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ const navBar = document.querySelector(".s-navigation-bar")
 const navBar_close = document.querySelector(".c-navigation-bar__close")
 
 
-navMenu.addEventListener('click', () => {
+navMenu.addEventListener('click', openNav )
+
+function openNav () {
     navBar.classList.add('s-navigation-bar__open')
-})
+}
 
 navBar_close.addEventListener('click', () => {
     navBar.classList.remove('s-navigation-bar__open')
@@ -20,10 +22,12 @@ const bookTableMenu = document.querySelector(".c-book-table")
 const reservationBar = document.querySelector(".s-book-reservation")
 const reservationBar_close = document.querySelector(".c-reservation__close")
 
-bookTableMenu.addEventListener('click', () => {
+bookTableMenu.addEventListener('click', (navMenu) => {
     reservationBar.classList.add('s-book-reservation__open')
+    navMenu.removeEventListener('click', openNav)
 })
 
 reservationBar_close.addEventListener('click', () => {
     reservationBar.classList.remove('s-book-reservation__open')
 })
+
