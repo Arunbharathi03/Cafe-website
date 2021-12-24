e3f4090c29d69f4639ee3f35f7ef7aa330ca86e5
Arunbharathi03 <arunaravind03@gmail.com>
2021-12-24 12:18:23 +0530

    added functionalities to navbar and booking menu

--- js/app.js
+++ js/app.js
@@ -1,33 +1,45 @@
+let bookTableTrigger = document.querySelector(".c-book-table")
+let reservationBar = document.querySelector(".s-book-reservation")
+let reservationBar_close = document.querySelector(".c-reservation__close")
+
+let navMenuTrigger = document.getElementById("nav-menu")
+let navBar = document.querySelector(".s-navigation-bar")
+let navBar_close = document.querySelector(".c-navigation-bar__close")
+
+// Reservation function
+bookTableTrigger.addEventListener('click', (e) => {
+    e.stopPropagation()
+    reservationBar.classList.add('s-book-reservation__open')
+    navMenuTrigger = false
+})
+
+reservationBar_close.addEventListener('click', (e) => {
+    reservationBar.classList.remove('s-book-reservation__open')
+    navMenuTrigger = true
+})
+
+reservationBar.addEventListener('click', function(e) {
+    e.stopPropagation()
+})
+
 // Navigation
 
-const navMenu = document.getElementById("nav-menu")
-const navBar = document.querySelector(".s-navigation-bar")
-const navBar_close = document.querySelector(".c-navigation-bar__close")
-
-
-navMenu.addEventListener('click', openNav )
-
-function openNav () {
-    navBar.classList.add('s-navigation-bar__open')
-}
+navMenuTrigger.addEventListener('click', (e) => {
+    e.stopPropagation()
+    if( navMenuTrigger)navBar.classList.add('s-navigation-bar__open')
+})
 
 navBar_close.addEventListener('click', () => {
     navBar.classList.remove('s-navigation-bar__open')
 })
 
-
-// Reservation function
-
-const bookTableMenu = document.querySelector(".c-book-table")
-const reservationBar = document.querySelector(".s-book-reservation")
-const reservationBar_close = document.querySelector(".c-reservation__close")
-
-bookTableMenu.addEventListener('click', (navMenu) => {
-    reservationBar.classList.add('s-book-reservation__open')
-    navMenu.removeEventListener('click', openNav)
+navBar.addEventListener('click', function(e) {
+    e.stopPropagation()
 })
 
-reservationBar_close.addEventListener('click', () => {
+
+window.addEventListener('click', function(){
+    navBar.classList.remove('s-navigation-bar__open')
     reservationBar.classList.remove('s-book-reservation__open')
+    navMenuTrigger = true
 })
-
